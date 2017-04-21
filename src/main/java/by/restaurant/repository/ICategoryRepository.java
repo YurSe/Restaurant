e9fc48f33db0288199883039b1b314876fd9b755
MIKL99999 <radykevich@gmail.com>
2017-04-21 20:32:35 +0300

    Add CategoryRepository

---
+++ src/main/java/by/restaurant/repository/ICategoryRepository.java
@@ -0,0 +1,5 @@
+import by.restaurant.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
+}
