3afc66b43a49f0dc4a5f44951cfed91e0727bf84
MIKL99999 <radykevich@gmail.com>
2017-04-22 16:33:08 +0300

    Add properties for Gmap

--- src/main/java/by/restaurant/controller/MarkerView.java
+++ src/main/java/by/restaurant/controller/MarkerView.java
@@ -1,16 +1,19 @@
 package by.restaurant.controller;
 
-
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
-@ManagedBean
+import java.util.Properties;
+
+@Controller
+@Scope("singleton")
 public class MarkerView implements Serializable {
 
     private MapModel simpleModel;
@@ -19,9 +22,12 @@ public class MarkerView implements Serializable {
     public void init() {
         simpleModel = new DefaultMapModel();
 
-        LatLng coord = new LatLng(53.684562, 23.840140);
+        Properties property = new Properties();
 
-        simpleModel.addOverlay(new Marker(coord, "Restaurant"));
+        LatLng coordinates = new LatLng(Integer.parseInt(property.getProperty("lat")),
+                Integer.parseInt(property.getProperty("lng")));
+
+        simpleModel.addOverlay(new Marker(coordinates, "Restaurant"));
     }
 
     public MapModel getSimpleModel() {
