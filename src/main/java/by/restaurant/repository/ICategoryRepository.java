f328158d54df41faadfc24c170239e7f8695aa67
MIKL99999 <radykevich@gmail.com>
2017-04-28 20:46:37 +0300

    Add function getCategoryByName

--- src/main/java/by/restaurant/repository/ICategoryRepository.java
+++ src/main/java/by/restaurant/repository/ICategoryRepository.java
@@ -1,5 +1,16 @@
+package by.restaurant.repository;
+
 import by.restaurant.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("select c from Category c where c.name = :name")
+    Category getCategoryByName(@Param("name") String name);
+
 }
