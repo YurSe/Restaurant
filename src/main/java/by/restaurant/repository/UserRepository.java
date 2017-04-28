811a52889bbf6f4d6e3d80c495965d1968c26370
MIKL99999 <radykevich@gmail.com>
2017-04-28 20:46:52 +0300

    Refactor

--- src/main/java/by/restaurant/repository/UserRepository.java
+++ src/main/java/by/restaurant/repository/UserRepository.java
@@ -4,9 +4,6 @@ import by.restaurant.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by Pavel on 11.04.2017.
- */
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
     User findByEmail(String email);
