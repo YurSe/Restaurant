78be702bba24889e4f91c30c36a377fd9973bfc0
MIKL99999 <radykevich@gmail.com>
2017-05-24 13:02:38 +0300

    Add function getUserByName

--- src/main/java/by/restaurant/repository/UserRepository.java
+++ src/main/java/by/restaurant/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package by.restaurant.repository;
 
 import by.restaurant.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("select u from User u where u.name = :name")
+    User getUserByName(@Param("name") String name);
+
     User findByEmail(String email);
 
     User findByPhoneNumber(String phoneNumber);
--- src/main/java/by/restaurant/service/implementation/UserServiceImpl.java
+++ src/main/java/by/restaurant/service/implementation/UserServiceImpl.java
@@ -4,18 +4,21 @@ import by.restaurant.model.User;
 import by.restaurant.repository.UserRepository;
 import by.restaurant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Created by Pavel on 15.04.2017.
- */
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public User getUserByName(String name) {
+        return userRepository.getUserByName(name);
+    }
 
     @Override
     public void registerUser(User user) throws Exception {
