bca5451885250a723f23351b5a2453faa6080891
MIKL99999 <radykevich@gmail.com>
2017-05-24 14:55:23 +0300

    Make order

--- src/main/java/by/restaurant/controller/OrderController.java
+++ src/main/java/by/restaurant/controller/OrderController.java
@@ -3,6 +3,7 @@ package by.restaurant.controller;
 import by.restaurant.model.Dish;
 import by.restaurant.model.Order;
 import by.restaurant.service.UserService;
+import by.restaurant.service.implementation.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,15 +24,22 @@ public class OrderController implements Serializable {
     @Autowired
     private UserService userService;
 
-    private Order order = new Order();
+    @Autowired
+    private OrderService orderService;
+
+    private Order order;
 
     @PostConstruct
     private void init() {
+        order = new Order();
+
         User userSpring = (org.springframework.security.core.userdetails.User) SecurityContextHolder
                 .getContext().getAuthentication().getPrincipal();
         by.restaurant.model.User user = userService.getUserByName(userSpring.getUsername());
         order.setUser(user);
+
         order.setGuestCount(1);
+
         Set<Dish> dishes = new HashSet<>();
         order.setDishes(dishes);
     }
@@ -50,4 +60,11 @@ public class OrderController implements Serializable {
     public void setOrder(Order order) {
         this.order = order;
     }
+
+    public void MakeOrder() {
+        order.setTimestamp(new Timestamp(new Date().getTime()));
+        orderService.save(order);
+        init();
+    }
+
 }
