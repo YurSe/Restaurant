4d0ecb81730a25bd6c998fc32d241edfb5f93bd7
MIKL99999 <radykevich@gmail.com>
2017-06-07 15:41:31 +0300

    Create Order_dish JpaRepository

---
+++ src/main/java/by/restaurant/repository/IOrder_dishRepository.java
@@ -0,0 +1,8 @@
+package by.restaurant.repository;
+
+import by.restaurant.model.Order_dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IOrder_dishRepository extends JpaRepository<Order_dish, Long> {
+
+}
